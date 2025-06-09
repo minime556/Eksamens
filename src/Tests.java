5cfbcf03a506b08aa1d4533cccf112c395b3b296
minime556 <markuss7kalnins@gmail.com>
2025-06-09 22:12:59 +0300

    Pievienots pirmā uzdevuma piemēra apraksts un rezultāts, un īstenots java programmā

--- src/Tests.java
+++ src/Tests.java
@@ -2,8 +2,6 @@
 public class Tests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- src/Uzd1.java
+++ src/Uzd1.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Uzd1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		//Simbola ievade
+		System.out.println("Ievadi vienu simbolu");
+		char simbols = scan.next().charAt(0);
+		scan.close();
+		
+        //Pārbauda vai simbols ir burts
+		if(Character.isLetter(simbols)) {
+			
+			//Pārbauda vai tas ri lielais burts
+			if(Character.isUpperCase(simbols)) {
+				System.out.println("Simbols ir lielais burts");
+			}else {
+				System.out.println("Simbols ir mazais burts");
+			}
+			//Pārbauda vai simbols ir cipars
+		}else if(Character.isDigit(simbols)) {
+			System.out.println("Simbols ir cipars");
+		}
+		else {
+			System.out.println("Simbols nav ne cipars ne burts");
+		}
 	}
-
 }
